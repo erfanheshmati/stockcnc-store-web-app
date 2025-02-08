f908e28c9dd75c2b8c9f990696ed09bab8925f06
Erfan Heshmati <heshmati74@gmail.com>
2025-02-09 00:21:02 +0330

    Update logo.tsx

--- components/shared/header/logo.tsx
+++ components/shared/header/logo.tsx
@@ -1,9 +1,39 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Logo2 from "../logo-2";
+import Logo1 from "../logo-1";
 
 export default function Logo() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 90) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="md:hidden lg:block absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-[2]">
-      <Logo2 />
+    <div
+      className={`${
+        isVisible ? "block md:hidden lg:block" : "md:hidden"
+      } absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-[2]`}
+    >
+      <img
+        src="/icons/stock-cnc-mobile.png"
+        alt="Logo"
+        className="hidden md:block w-[130px]"
+      />
+      <Logo2 className="block md:hidden" />
     </div>
   );
 }
