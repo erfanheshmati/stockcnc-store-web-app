af4fd0a32131778f6235992f789bada0a95e9904
Erfan Heshmati <heshmati74@gmail.com>
2025-02-18 13:16:09 +0330

    Add gallery carousel to product page

--- app/(root)/product/[slug]/product-images.tsx
+++ app/(root)/product/[slug]/product-images.tsx
@@ -1,16 +1,17 @@
 "use client";
 
 // import Swiper core and required modules, styles
-import { Pagination } from "swiper/modules";
+import { Pagination, Navigation, FreeMode } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 
 import { useState } from "react";
-import { BiX } from "react-icons/bi";
+import { BiChevronLeft, BiChevronRight, BiX } from "react-icons/bi";
 import { SlSizeFullscreen } from "react-icons/sl";
 import { IMAGE_URL } from "@/lib/constants";
 import { Product } from "@/lib/types";
+import Link from "next/link";
 
 export default function ProductImages({ data }: { data: Product }) {
   const [current, setCurrent] = useState(0);
@@ -54,44 +55,117 @@ export default function ProductImages({ data }: { data: Product }) {
       {/* Desktop View */}
       <div className="hidden md:flex flex-col gap-6 relative">
         {/* Main Image */}
-        <div
-          className={`${
-            isFullScreen
-              ? "flex justify-center items-center fixed inset-0 bg-black z-50"
-              : ""
-          }`}
-        >
-          <div
-            className={`flex items-center justify-center bg-secondary/10 rounded-xl relative ${
-              !isFullScreen && "md:h-[640px] lg:h-[420px] border"
-            }`}
-          >
-            <img
-              src={`${IMAGE_URL}/720${data.gallery[current]}`}
-              alt="Product Image"
-              className="w-full h-full rounded-xl"
-            />
-          </div>
-          {isFullScreen && (
-            <button
-              onClick={() => setIsFullScreen(false)}
-              className="absolute top-4 right-4 hover:opacity-60 transition-all duration-300 ease-in-out z-10"
-            >
-              <BiX size={24} color="white" />
-            </button>
-          )}
-          {!isFullScreen && (
-            <button
-              onClick={() => setIsFullScreen(true)}
-              className="group absolute top-4 left-4 bg-primary p-2 rounded-full z-10"
-            >
-              <SlSizeFullscreen
-                size={14}
-                className="text-white group-hover:opacity-80 transition-all duration-300 ease-in-out"
-              />
-            </button>
-          )}
+        <div className="flex items-center justify-center bg-secondary/10 rounded-xl md:h-[640px] lg:h-[420px] border relative">
+          <img
+            src={`${IMAGE_URL}/${data.gallery[current]}`}
+            alt="Product Image"
+            className="w-full h-full rounded-xl"
+          />
         </div>
+        {/* Set Fullscreen Button */}
+        {!isFullScreen && (
+          <button
+            onClick={() => setIsFullScreen(true)}
+            className="group absolute top-4 left-4 bg-primary p-2 rounded-full z-10"
+          >
+            <SlSizeFullscreen
+              size={14}
+              className="text-white group-hover:opacity-80 transition-all duration-300 ease-in-out"
+            />
+          </button>
+        )}
+        {/* Fullscreen View */}
+        {isFullScreen && (
+          <div className="flex flex-col h-screen fixed inset-0 bg-black z-50">
+            <div className="flex items-center justify-between px-12 py-8">
+              <button
+                onClick={() => setIsFullScreen(false)}
+                className="hover:opacity-60 transition-all duration-300 ease-in-out"
+              >
+                <BiX size={30} color="white" />
+              </button>
+              <Link
+                href="/"
+                className="text-white text-xl font-bold tracking-wider font-sans"
+              >
+                stockcnc.com
+              </Link>
+            </div>
+            {/* Slideshow */}
+            <div className="flex flex-col justify-center h-full relative">
+              {/* Custom arrows */}
+              <div className="custom-product-carousel-button-prev left-3 bg-neutral-400/80 hover:bg-neutral-300 transition-all duration-300 ease-in-out flex items-center justify-center rounded-full cursor-pointer w-9 h-9 z-[5]">
+                <BiChevronLeft size={24} />
+              </div>
+              <div className="custom-product-carousel-button-next right-3 bg-neutral-400/80 hover:bg-neutral-300 transition-all duration-300 ease-in-out flex items-center justify-center rounded-full cursor-pointer w-9 h-9 z-[5]">
+                <BiChevronRight size={24} />
+              </div>
+              {/* Swiper */}
+              <div>
+                <Swiper
+                  modules={[Navigation, Pagination, FreeMode]}
+                  slidesPerView={1}
+                  spaceBetween={0}
+                  centeredSlides={true}
+                  loop={true}
+                  pagination={{
+                    clickable: true,
+                    el: ".custom-product-carousel-pagination",
+                    bulletClass: "custom-product-carousel-pagination-bullet",
+                    bulletActiveClass:
+                      "custom-product-carousel-pagination-bullet-active",
+                  }}
+                  navigation={{
+                    prevEl: ".custom-product-carousel-button-prev",
+                    nextEl: ".custom-product-carousel-button-next",
+                  }}
+                  breakpoints={{
+                    768: {
+                      slidesPerView: 1,
+                      spaceBetween: -250,
+                    },
+                    1024: {
+                      slidesPerView: 1,
+                      spaceBetween: -400,
+                    },
+                    1280: {
+                      slidesPerView: 1,
+                      spaceBetween: -600,
+                    },
+                    1360: {
+                      slidesPerView: 1,
+                      spaceBetween: -700,
+                    },
+                    1536: {
+                      slidesPerView: 1,
+                      spaceBetween: -650,
+                    },
+                    1680: {
+                      slidesPerView: 1,
+                      spaceBetween: -800,
+                    },
+                    1840: {
+                      slidesPerView: 1,
+                      spaceBetween: -900,
+                    },
+                  }}
+                >
+                  {data.gallery.map((image, index) => (
+                    <SwiperSlide className="pb-10" key={index}>
+                      <img
+                        src={`${IMAGE_URL}/${image}`}
+                        alt="Product Image"
+                        className="md:h-[400px] lg:h-[450px] xl:h-[500px] 2xl:h-[660px] mx-auto"
+                      />
+                    </SwiperSlide>
+                  ))}
+                </Swiper>
+              </div>
+              {/* Custom pagination container */}
+              <div className="custom-product-carousel-pagination"></div>
+            </div>
+          </div>
+        )}
 
         {/* Thumbnails */}
         <div
