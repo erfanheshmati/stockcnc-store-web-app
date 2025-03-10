acd7ce1ccee04f18612934ca273ca2637da6c392
Erfan Heshmati <heshmati74@gmail.com>
2025-03-10 15:25:00 +0330

    Update phone link in header

--- components/shared/header/info.tsx
+++ components/shared/header/info.tsx
@@ -27,14 +27,14 @@ export default function Info() {
     <>
       {/* Mobile Info */}
       <div className="md:hidden flex items-center text-white z-[9]">
-        <Link href="tel:+982122954000">
+        <Link href={`tel:${supportPhone}`}>
           <BiSupport size={24} />
         </Link>
       </div>
 
       {/* Desktop Info */}
       <div className="hidden md:flex text-white hover:text-white/70 z-10 transition-all duration-300 ease-in-out">
-        <Link href="tel:+982122954000" className="flex items-center gap-4 ">
+        <Link href={`tel:${supportPhone}`} className="flex items-center gap-4 ">
           <span className="font-bold text-[14px]">تلفن مشاوره</span>
           {error && <span className="text-red-500 pt-1">{error}</span>}
           <span className="font-bold text-[18px]">
