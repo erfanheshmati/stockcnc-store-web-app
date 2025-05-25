6c0c072bbb23e1a5ab6f6783126b50713f3d9238
Erfan Heshmati <heshmati74@gmail.com>
2025-05-25 21:30:51 +0330

    Prevent scrolling when sort pop-up is open

--- components/shared/dialog-box-sort.tsx
+++ components/shared/dialog-box-sort.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import SortMobile from "@/app/(root)/archiv/sort-mobile";
+import { useEffect } from "react";
 
 interface DialogBoxSortProps {
   isOpen: boolean;
@@ -6,13 +9,23 @@ interface DialogBoxSortProps {
 }
 
 const DialogBoxSort: React.FC<DialogBoxSortProps> = ({ isOpen, onClose }) => {
-  if (!isOpen) return null;
+  // Prevent scrolling when pop-up is open
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [isOpen]);
 
-  return (
+  return isOpen ? (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-60 px-4 sm:px-0 z-20">
       <SortMobile onClose={onClose} />
     </div>
-  );
+  ) : null;
 };
 
 export default DialogBoxSort;
