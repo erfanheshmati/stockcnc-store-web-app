17154765b911560c4e3a64b6723fb6ea1124fb51
Erfan Heshmati <heshmati74@gmail.com>
2025-05-25 21:18:21 +0330

    Prevent scrolling when inquiry pop-up is open

--- components/shared/dialog-box-inquiry.tsx
+++ components/shared/dialog-box-inquiry.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import InquiryForm from "@/components/shared/inquiry-form";
+import { useEffect } from "react";
 
 interface DialogBoxInquiryProps {
   isOpen: boolean;
@@ -9,13 +12,23 @@ const DialogBoxInquiry: React.FC<DialogBoxInquiryProps> = ({
   isOpen,
   onClose,
 }) => {
-  if (!isOpen) return null;
+  // Prevent scrolling when pop-up is open
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [isOpen]);
 
-  return (
+  return isOpen ? (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-60 px-4 sm:px-0 z-20">
       <InquiryForm onClose={onClose} />
     </div>
-  );
+  ) : null;
 };
 
 export default DialogBoxInquiry;
